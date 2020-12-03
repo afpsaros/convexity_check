c0b4b1e52c3e90962a17abcb71e431d0cb33b09f
afpsaros <70901418+afpsaros@users.noreply.github.com>
2020-12-02 20:02:41 -0500

    ni

---
+++ conftest.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 17:45:56 2020
+
+@author: afpsaros
+"""
+
+
+collect_ignore = ['setup.py']
--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,11 @@
 import numpy as np
-from convexity_check import is_not_concave, is_not_convex
+from checker import is_not_concave, is_not_convex
 
 def convex_f(x):
     return x[:,0]**2+x[:,1]**2
+
+def convex_f_1(x):
+    return x[:,0]**3
 
 def concave_f(x):
     return -x[:,0]**2-x[:,1]**2
@@ -12,16 +15,32 @@ def bohachevsky1(x):
 def bohachevsky2(x):
     return x[:,0]**2+2*x[:,1]**2-0.3*np.cos(3*np.pi*x[:,0])*np.cos(4*np.pi*x[:,1])+0.3
 
-bounds = np.array([[0,0],[1,1]])
-assert is_not_concave(bohachevsky1,2,bounds)
-assert is_not_convex(bohachevsky1,2,bounds)
-assert is_not_concave(bohachevsky2,2,bounds)
-assert is_not_convex(bohachevsky2,2,bounds)
+bounds = np.array([[0],[1]])
+
+# assert is_not_concave(convex_f,2,bounds)
+#%%
+# assert is_not_convex(convex_f,2,bounds)
+#%%
+
+# assert is_not_concave(bohachevsky1,2,bounds)
+# assert is_not_convex(bohachevsky1,2,bounds)
+# assert is_not_concave(bohachevsky2,2,bounds)
+# assert is_not_convex(bohachevsky2,2,bounds)
+# =============================================================================
+# try: 
+#     assert is_not_convex(convex_f,2,bounds)
+# except:
+#     print('possibly convex')
+#     pass
+# # try: 
+# =============================================================================
+#     assert not is_not_concave(concave_f,2,bounds)
+# except:
+#     pass
+
 try: 
-    assert not is_not_convex(convex_f,2,bounds)
+    assert is_not_convex(convex_f_1,1,bounds)
+    print('not convex')
 except:
-    pass
-try: 
-    assert not is_not_concave(concave_f,2,bounds)
-except:
+    print('possibly convex')
     pass
